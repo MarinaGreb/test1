fda204ad9950b52b7e4af7e374d9ef6e27ca5942
marinagrebneva <sokovets@outlook.com>
2022-10-15 18:48:30 +0200

    added all files of project

---
+++ test.js
@@ -0,0 +1,12 @@
+//let x = 1;
+//let y = 2;
+//console.log(x + y);
+const sayHelloLinting = (fName) => {
+  console.log(`Look, how pretty it is, ${fName}`);
+};
+sayHelloLinting("Oksana");
+
+
+//const sayHelloLinting = (fName) => {console.log(`Look, how pretty it is, ${fName}`);};
+
+//sayHelloLinting('Oksana');
