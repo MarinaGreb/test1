0173c58b28787b7513107be6c3afe2f8f35c8bcf
marinagrebneva <sokovets@outlook.com>
2022-10-16 13:13:59 +0200

    changed name

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
 const sayHelloLinting = (fName) => {
   console.log(`Look, how pretty it is, ${fName}`);
 };
-sayHelloLinting("Marina");
+sayHelloLinting("Ksenia");
 
 
 //const sayHelloLinting = (fName) => {console.log(`Look, how pretty it is, ${fName}`);};
